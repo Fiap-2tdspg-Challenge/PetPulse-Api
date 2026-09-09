c58b587f4e452c215d842647ac66c5ce850236b0
PietroWilhelm <pietrowilhelm@git.com>
2026-09-09 20:17:48 -0300

    feat: atualizei o tempo de expiração do token para 20 minutos

--- src/main/java/fiap/com/br/petpulse/service/TokenService.java
+++ src/main/java/fiap/com/br/petpulse/service/TokenService.java
@@ -31,7 +31,7 @@ public class TokenService {
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(now)
-                .expiresAt(now.plus(2, ChronoUnit.MINUTES))
+                .expiresAt(now.plus(20, ChronoUnit.MINUTES))
                 .subject(authentication.getName())
                 .claim("role", role)
                 .build();
